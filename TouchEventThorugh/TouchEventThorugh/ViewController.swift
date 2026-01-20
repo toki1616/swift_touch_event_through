ad4f44f9ed40ca7c87808f8f3def8e66d1ea1910
toki1616 <tokigit@gmail.com>
2026-01-20 22:21:17 +0900

    Projectの追加

---
+++ TouchEventThorugh/TouchEventThorugh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TouchEventThorugh
+//
+//  Created by taiki toyoda on 2026/01/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TouchEventThorugh/TouchEventThorughTests/TouchEventThorughTests.swift
@@ -0,0 +1,17 @@
+//
+//  TouchEventThorughTests.swift
+//  TouchEventThorughTests
+//
+//  Created by taiki toyoda on 2026/01/20.
+//
+
+import Testing
+@testable import TouchEventThorugh
+
+struct TouchEventThorughTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
